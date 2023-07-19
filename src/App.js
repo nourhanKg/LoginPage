3423d0ae357c2b0c6aa27204f1a72ae1507ac996
Nourhan Ahmed AboElenen <36276898+nourhanKg@users.noreply.github.com>
2023-07-19 15:29:34 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import LoginPage from "./components/Pages/LoginPage";
-import "./styles/main-ltr.css";
-// import { useEffect } from "react";
-// import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 
 function App() {
+  const { i18n } = useTranslation();
+  const [stylesheet, setStylesheet] = useState("./styles/main-ltr.css");
+
+  useEffect(() => {
+    if (i18n.language === "ar") {
+      setStylesheet("./styles/main-rtl.css");
+    } else {
+      setStylesheet("./styles/main-ltr.css");
+    }
+  }, [i18n.language]);
+
   return (
     <>
-      <LoginPage></LoginPage>
+      <Helmet>
+        <link rel="stylesheet" href={stylesheet} />
+      </Helmet>
+      <LoginPage />
     </>
   );
 }
